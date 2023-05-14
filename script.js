d46d027d3e742ee707310abac92e7c08d240d74b
Santhosh Kumar <iamsanthosh2203@gmail.com.com>
2023-05-14 18:27:50 +0530

    Calc-App

---
+++ script.js
@@ -0,0 +1,25 @@
+const buttons = document.querySelectorAll(".button");
+const output = document.querySelector("#output");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const value = button.value;
+    output.value += value;
+  });
+});
+
+const clearButton = document.querySelectorAll(".buttonClear");
+
+clearButton.forEach((button) => {
+  button.addEventListener("click", () => {
+    output.value = "";
+  });
+});
+
+
+const calButton = document.getElementById("calc");
+
+calButton.addEventListener("click",function(){
+    let answer = eval(output.value)
+    output.value=answer;
+})
